4f9d8db133c64e92af12aca1f9520abdd2538985
1stApr <anhtuan01041998@gmail.com>
2020-12-05 10:37:27 +0700

    create layout and add art

---
+++ app/src/main/java/com/sinon/clockwidget/ClockChoice.java
@@ -0,0 +1,14 @@
+package com.sinon.clockwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClockChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_choice);
+    }
+}
---
+++ app/src/main/java/com/sinon/clockwidget/ClockWidget.java
@@ -0,0 +1,13 @@
+package com.sinon.clockwidget;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ClockWidget extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+    }
+}
